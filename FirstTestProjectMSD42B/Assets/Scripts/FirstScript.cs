5f054b30ddc49f7cf9fdbfa67ea80cd13e6aa2ec
Bungy21 <lian.bugeja.e22686@mcast.edu.mt>
2020-10-21 16:23:48 +0200

    Created FirstScript
    Difference between Start() and Update()

---
+++ FirstTestProjectMSD42B/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Welcome MSD4.2B");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        print("Hello everyone!!");
+    }
+}
