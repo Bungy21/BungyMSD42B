b02f5d28a2c8dd27d261bde6071fcf7b421f6264
Bungy21 <lian.bugeja.e22686@mcast.edu.mt>
2020-10-28 16:27:29 +0100

    Update1Laser

--- LaserDefender/LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/LaserDefender/Assets/Scripts/Player.cs
@@ -13,6 +13,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        //var is a generic variable which changes its type according to value
+        //deltaX is the difference the Player Ship moves in the x-axis
+        var deltaX = Input.GetAxis("Horizontal");
+
+        //newXPosition = current pos in x + difference moved in x-axis
+        var newXPos = transform.position.x + deltaX;
+
+        //move the Player Ship on the x-axis only (newXPos)
+        transform.position = new Vector2(newXPos, transform.position.y);
     }
 }
