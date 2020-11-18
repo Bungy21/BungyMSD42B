2c3ec7e4e1f5a472c48e5c9dd363eed78c7f05a2
Bungy21 <lian.bugeja.e22686@mcast.edu.mt>
2020-11-18 16:19:32 +0100

    Waves

--- LaserDefender/LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
+    //a list of type Transform as waypoints are positions in x and y   
     [SerializeField] List<Transform> waypointsList;
+
+    [SerializeField] float enemyMoveSpeed = 2f;
+    
+    //shows the next waypoint 
+    int waypointIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +22,36 @@ public class EnemyPathing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        // 0, 
+        if (waypointIndex < waypointsList.Count)
+        {
+            //set the targetPosition to the next waypoint Position
+            //targetposition: where we want to go
+            var targetPosition = waypointsList[waypointIndex].transform.position;
+
+            targetPosition.z = 0f;
+
+            var enemyMovement = enemyMoveSpeed * Time.deltaTime;
+
+            //move from current position, to target position, at enemyMovement speed
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemyMovement);
+
+            // check if we reached targetPosition
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        //if enemy reached last     waypoint
+        else
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
---
+++ LaserDefender/LaserDefender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    //the enemy sprite
+    [SerializeField] GameObject enemyPrefab;
+
+    //the path to follow
+    [SerializeField] GameObject pathPrefab;
+    //time between enemy spawn
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    //random time difference between spawns
+    [SerializeField] float spawnRandomfactor = 0.3f;
+    //number of enemies in wave
+    [SerializeField] int numberOfEnemies = 5;
+    //enemy movement speed
+    [SerializeField] float enemyMoveSpeed = 2f;
+}
